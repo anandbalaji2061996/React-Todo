af08390c91aa59676050312a56b5492253db66af
Anand Balaji Prakash Kumar (RBEI/PJ-IOT-IMPL3) <npn1cob@bosch.com>
2020-07-10 10:15:44 +0530

    Updates

---
+++ precache-manifest.5380158a564c0d93d85031470c043fed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5308358c4e8eab8f09c16093303d04",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "03c9b522bbb0455dbe2a",
+    "url": "/react-todo-app/static/css/main.6c14820b.chunk.css"
+  },
+  {
+    "revision": "17981fbfbf290d7d4f25",
+    "url": "/react-todo-app/static/js/2.ce3adfb3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo-app/static/js/2.ce3adfb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03c9b522bbb0455dbe2a",
+    "url": "/react-todo-app/static/js/main.ce429093.chunk.js"
+  },
+  {
+    "revision": "0e0ffa60d2a6a6983e78",
+    "url": "/react-todo-app/static/js/runtime-main.f96e123f.js"
+  }
+]);
