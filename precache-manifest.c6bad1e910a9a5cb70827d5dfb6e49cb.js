a1a75d059350c38acc1a35fa544684145dcfb730
Anand Balaji Prakash Kumar (RBEI/PJ-IOT-IMPL3) <npn1cob@bosch.com>
2020-07-10 10:29:43 +0530

    Updates

---
+++ precache-manifest.c6bad1e910a9a5cb70827d5dfb6e49cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1779d2b08c4996f9f731da9b5a9450",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "03c9b522bbb0455dbe2a",
+    "url": "/react-todo/static/css/main.6c14820b.chunk.css"
+  },
+  {
+    "revision": "17981fbfbf290d7d4f25",
+    "url": "/react-todo/static/js/2.ce3adfb3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo/static/js/2.ce3adfb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03c9b522bbb0455dbe2a",
+    "url": "/react-todo/static/js/main.ce429093.chunk.js"
+  },
+  {
+    "revision": "f7d85b2af89dbea0717b",
+    "url": "/react-todo/static/js/runtime-main.b7e0cced.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-todo-app/precache-manifest.5380158a564c0d93d85031470c043fed.js"
+  "/react-todo/precache-manifest.c6bad1e910a9a5cb70827d5dfb6e49cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-todo-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
